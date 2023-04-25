c8cb60378da93480ef035c264e1a8c0c4d016a5f
dolezj42 <dolezj42@fit.cvut.cz>
2023-04-25 12:00:42 +0200

    add phone

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -2,6 +2,7 @@ import {Document} from 'mongoose';
 interface User extends Document {
   user_name: string;
   email: string;
+  phone: number;
   password: string;
 }
 
@@ -10,6 +11,7 @@ interface UserTest {
   user_name?: string; // returned from graphql is snake_case
   userName?: string; // graphql variables are camelCase
   email?: string;
+  phone?: number;
   password?: string;
   token?: string;
 }
